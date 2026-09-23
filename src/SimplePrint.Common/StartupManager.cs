7cd68ad0f49bc324e7d36dbd1bf66c7c90ca8088
tojollinor <109473660+tojollinor@users.noreply.github.com>
2026-09-23 12:29:55 +0200

    Improve autostart state handling

--- src/SimplePrint.Common/StartupManager.cs
+++ src/SimplePrint.Common/StartupManager.cs
@@ -7,26 +7,40 @@ public static class StartupManager
     private const string RunKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
 
     public static bool IsSystemWideEnabled(string valueName)
+        => GetSystemWideCommand(valueName) is not null;
+
+    public static string? GetSystemWideCommand(string valueName)
     {
         try
         {
             using var baseKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
             using var key = baseKey.OpenSubKey(RunKey, writable: false);
-            return key?.GetValue(valueName) is string value && !string.IsNullOrWhiteSpace(value);
+            return key?.GetValue(valueName) as string;
         }
         catch
         {
-            return false;
+            return null;
         }
     }
 
-    public static Task SetSystemWideAsync(string valueName, string executablePath, bool enabled)
+    public static bool IsTrayModeEnabled(string valueName, bool defaultValue = true)
+    {
+        var command = GetSystemWideCommand(valueName);
+        if (string.IsNullOrWhiteSpace(command)) return defaultValue;
+        return command.Contains("--tray", StringComparison.OrdinalIgnoreCase);
+    }
+
+    public static Task SetSystemWideAsync(string valueName, string executablePath, bool enabled, bool startInTray = true)
     {
         var key = @"HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
         string script;
+
         if (enabled)
         {
-            var command = $"\"{executablePath}\" --tray";
+            var command = startInTray
+                ? $"\"{executablePath}\" --tray"
+                : $"\"{executablePath}\"";
+
             script = $@"
 $ErrorActionPreference='Stop'
 New-Item -Path {PowerShellRunner.Quote(key)} -Force | Out-Null
