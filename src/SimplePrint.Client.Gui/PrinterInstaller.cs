63c609341ca6befc620f5067cc6619986b798349
tojollinor <109473660+tojollinor@users.noreply.github.com>
2026-09-23 15:11:12 +0200

    Fix invalid SNMP parameter when creating printer port

--- src/SimplePrint.Client.Gui/PrinterInstaller.cs
+++ src/SimplePrint.Client.Gui/PrinterInstaller.cs
@@ -25,7 +25,7 @@ if($existing -and $existing.PortName -ne $port) {{
 }}
 
 if(-not (Get-PrinterPort -Name $port -ErrorAction SilentlyContinue)) {{
-  Add-PrinterPort -Name $port -PrinterHostAddress '127.0.0.1' -PortNumber {mapping.LocalProxyPort} -SNMP 0
+  Add-PrinterPort -Name $port -PrinterHostAddress '127.0.0.1' -PortNumber {mapping.LocalProxyPort}
 }}
 
 if($existing) {{
