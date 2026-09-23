3b8216ab37f88b57b1e3fcb8517e0dde950c0c99
tojollinor <109473660+tojollinor@users.noreply.github.com>
2026-09-23 16:02:42 +0200

    Refresh SimplePrint about dialog branding

--- src/SimplePrint.Client.Gui/AboutForm.cs
+++ src/SimplePrint.Client.Gui/AboutForm.cs
@@ -1,16 +1,21 @@
+using System.Diagnostics;
+
 namespace SimplePrint.Client.Gui;
 
 internal sealed class AboutForm : Form
 {
+    private const string RepositoryUrl = "https://github.com/tojollinor/SimplePrint";
+
     public AboutForm()
     {
         Text = "Über SimplePrint";
-        ClientSize = new Size(500, 270);
+        ClientSize = new Size(560, 320);
         StartPosition = FormStartPosition.CenterParent;
         FormBorderStyle = FormBorderStyle.FixedDialog;
         MaximizeBox = false;
         MinimizeBox = false;
         ShowInTaskbar = false;
+
         Branding.ApplyApplicationIcon(this);
 
         var assemblyVersion = typeof(AboutForm).Assembly.GetName().Version;
@@ -22,52 +27,86 @@ internal sealed class AboutForm : Form
         {
             Left = 24,
             Top = 24,
-            Width = 150,
-            Height = 150,
-            Padding = new Padding(8),
+            Width = 180,
+            Height = 180,
+            Padding = new Padding(6),
             SizeMode = PictureBoxSizeMode.Zoom,
             Image = Branding.LoadFixedAboutLogo()
         };
 
         var title = new Label
         {
-            Left = 195,
+            Left = 225,
             Top = 36,
-            Width = 270,
-            Height = 40,
+            Width = 305,
+            Height = 42,
             Text = "SimplePrint Client",
             Font = new Font(Font.FontFamily, 18, FontStyle.Bold)
         };
 
         var version = new Label
         {
-            Left = 198,
-            Top = 84,
-            Width = 260,
+            Left = 228,
+            Top = 86,
+            Width = 300,
             Height = 24,
-            Text = $"Version {versionText}"
+            Text = $"Version {versionText} · Protokoll {SimplePrint.Common.Protocol.Version}"
         };
 
         var description = new Label
         {
-            Left = 198,
-            Top = 120,
-            Width = 270,
-            Height = 64,
-            Text = "Lokaler RAW-Drucktransport\r\nmit automatischer Servererkennung."
+            Left = 228,
+            Top = 126,
+            Width = 300,
+            Height = 70,
+            Text = "Lokaler RAW-Drucktransport\nmit automatischer Servererkennung und End-to-End-Diagnose."
+        };
+
+        var copyright = new Label
+        {
+            Left = 28,
+            Top = 224,
+            Width = 500,
+            Height = 22,
+            Text = "© 2026 SimplePrint"
+        };
+
+        var link = new LinkLabel
+        {
+            Left = 28,
+            Top = 248,
+            Width = 500,
+            Height = 24,
+            Text = "github.com/tojollinor/SimplePrint",
+            AutoSize = false
+        };
+
+        link.LinkClicked += (_, _) =>
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = RepositoryUrl,
+                    UseShellExecute = true
+                });
+            }
+            catch
+            {
+            }
         };
 
         var close = new Button
         {
             Text = "Schließen",
-            Left = 365,
-            Top = 218,
+            Left = 425,
+            Top = 272,
             Width = 105,
             Height = 32,
             DialogResult = DialogResult.OK
         };
 
-        Controls.AddRange([logo, title, version, description, close]);
+        Controls.AddRange([logo, title, version, description, copyright, link, close]);
         AcceptButton = close;
         CancelButton = close;
     }
