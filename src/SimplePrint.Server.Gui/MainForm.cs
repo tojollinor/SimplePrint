0bedc7c92f17d707a020536fdc074e28ba3e2334
tojollinor <109473660+tojollinor@users.noreply.github.com>
2026-09-24 09:41:19 +0200

    Use server-specific update package

--- src/SimplePrint.Server.Gui/MainForm.cs
+++ src/SimplePrint.Server.Gui/MainForm.cs
@@ -319,7 +319,7 @@ public sealed class MainForm : Form
                 SetBusy("GitHub Releases werden geprüft …");
 
             var current = typeof(MainForm).Assembly.GetName().Version ?? new Version(0, 0, 0, 0);
-            var update = await GitHubUpdateService.CheckAsync(current);
+            var update = await GitHubUpdateService.CheckAsync(current, SimplePrintComponent.Server);
 
             if (update is null)
             {
